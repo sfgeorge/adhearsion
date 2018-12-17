07ddeb291ba516c3a5c225b4dc34e96837e8eaa2
kares <self@kares.org>
2018-12-17 21:01:47 +0100

    Implement interface for multiple output docs
    invoking new UniMRCP capabilities
    
    cloudvox/punchblock@b26180acf565231219b2e438386d3122bfcab364

--- lib/adhearsion/translator/asterisk/component/mrcp_prompt.rb
+++ lib/adhearsion/translator/asterisk/component/mrcp_prompt.rb
@@ -16,12 +16,11 @@ module Adhearsion
             raise OptionError, "The renderer #{renderer} is unsupported." unless renderer == 'unimrcp' or renderer == 'native_or_unimrcp'
             raise OptionError, "The recognizer #{recognizer} is unsupported." unless recognizer == 'unimrcp' or renderer == 'native_or_unimrcp'
             raise OptionError, 'An SSML document is required.' unless output_node.render_documents.count > 0
-            raise OptionError, 'Only one document is allowed.' if output_node.render_documents.count > 1
             raise OptionError, 'A grammar is required.' unless input_node.grammars.count > 0
 
 
             begin
-              render_doc
+              render_docs
             rescue => e
               logger.error e
               raise OptionError, 'The requested render document could not be parsed.'
@@ -39,12 +38,15 @@ module Adhearsion
           end
 
           def execute_unimrcp_app
-            execute_app 'SynthAndRecog', render_doc, grammars
+            execute_app 'SynthAndRecog', render_docs, grammars
           end
 
-          def render_doc
-            @render_doc ||= begin
-              d = output_node.render_documents.first
+          def render_docs
+            @render_docs ||= collect_docs.map(&:squish).join("^")
+          end
+
+          def collect_docs
+            output_node.render_documents.map do |d|
               if d.content_type
                 d.value.to_doc.to_s
               else
